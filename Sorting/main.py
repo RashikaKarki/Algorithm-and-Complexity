536a0286987af1c3076d9c0615e180630317404b
unknown <rashikakarki9841@gmail.com>
2020-11-08 18:29:06 +0545

    Fix tests

--- Sorting/main.py
+++ Sorting/main.py
@@ -22,13 +22,15 @@ def insertion_sort(list):
                         i = i-1
                 else:
                         break
+        return list
 
 def mergesort(data,p,r):
         if(p<r):
                 q = (p+r)//2
-                mergesort(data,p,q)
-                mergesort(data,q+1,r)
-                merged(data,p,q,r)
+                data = mergesort(data,p,q)
+                data = mergesort(data,q+1,r)
+                data = merged(data,p,q,r)
+        return data
         
 
 def merged(data,p,q,r):
@@ -46,25 +48,26 @@ def merged(data,p,q,r):
                         if(k<len(data)):
                                 data[k] = R[j]
                                 j+=1
+        return data
 
 def insertion():
-     for i in range(100,1000,10):
-        list = random.sample(range(1000),i)
-        start = time.time()
-        insertion_sort(list)
-        end = time.time()
-        t = end - start
-        insert_time.append(t)
-        size.append(i)
+        for i in range(100,1000,10):
+                list = random.sample(range(1000),i)
+                start = time.time()
+                sorted_list = insertion_sort(list)
+                end = time.time()
+                t = end - start
+                insert_time.append(t)
+                size.append(i)
 
 def merge():
-     for i in range(100,1000,10):
-        list = random.sample(range(1000),i)
-        start = time.time()
-        mergesort(list,0,len(list))
-        end = time.time()
-        t = end - start
-        merge_sort_time.append(t)
+        for i in range(100,1000,10):
+                list = random.sample(range(1000),i)
+                start = time.time()
+                sorted_list = mergesort(list,0,len(list))
+                end = time.time()
+                t = end - start
+                merge_sort_time.append(t)
         
 
 insertion()
